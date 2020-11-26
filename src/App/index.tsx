4c5d6bd5efcd18ad740502d0a40abdcaeb342b13
David Bradshaw <david.bradshaw@ibm.com>
2020-11-25 19:51:07 -0500

    fix: clean and include datasets

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,7 +4,22 @@ import React from 'react';
 export default class App extends React.Component {
   render(): React.ReactNode {
     return (
-      <h1>It works!</h1>
+      <div className="main-layout">
+        <h1>Title</h1>
+        <p>Intro text</p>
+        <h3>GRAPH Total accidents over time GRAPH</h3>
+        <p>Summary of graph above</p>
+        <h3>GRAPH Time based graphics GRAPH</h3>
+        <p>Hypothesis on how time alters results</p>
+        <h2>Vizion Zero</h2>
+        <p>What is Vision Zero</p>
+        <h3>GRAPH Vision zero laws and time GRAPH</h3>
+        <h2>How New York City Compares to other Cities</h2>
+        <p>Text about how other cities do it and their performance</p>
+        <h3>GRAPH City comparison GRAPH</h3>
+        <h2>Conclusion</h2>
+        <p>Conclusion text</p>
+      </div>
     );
   }
 }
